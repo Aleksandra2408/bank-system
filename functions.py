6c16a3f89011589f9454d6ccb3f1badc3c4fffc6
AleksandraLyashchenco <juicyluck007@gmail.com>
2021-06-02 22:38:34 +0300

    Account

--- functions.py
+++ functions.py
@@ -31,7 +31,7 @@ class Account:
         else:
             return False
 
-    def print(self, print_balance = False):
+    def print(self, print_balance=False):
         print('')
         print('Your card number:')
         print(self.card_number)
@@ -43,6 +43,25 @@ class Account:
         print('')
 
 
+def create_card_number():
+    card_number = list('400000' + str(int(random.randint(100000000, 999999999))))
+    c = list.copy(card_number)
+    for i in range(len(c)):
+        old_value = c[i]
+        new_value = int(old_value)
+        c[i] = new_value
+    for index, i in enumerate(c):
+        if index % 2 == 0:
+            i = i * 2
+        c[index] = i
+        if c[index] > 9:
+            c[index] = c[index] - 9
+    a = 0
+    if sum(c) % 10 > 0:
+        a = abs(10 - (sum(c) % 10))
+    card_number.append(a)
+    card_number = int("".join(map(str, card_number)))
+    return card_number
 # after all tasks: insert or update, print
 
 # create first account
@@ -56,3 +75,9 @@ fa.income(1000)
 fa.print(True)
 
 # transfer from fa to sa 500
+fa.output(500)
+sa.income(500)
+fa.print(True)
+sa.print(True)
+
+
